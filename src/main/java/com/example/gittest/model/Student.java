736b913b823776e7067b37d3b8c019406506e2b4
vanarenan <pserodaks@gmail.com>
2021-04-29 19:21:10 +0300

    Initial commit

---
+++ src/main/java/com/example/gittest/model/Student.java
@@ -0,0 +1,15 @@
+package com.example.gittest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Student {
+    String name;
+    int age;
+}
